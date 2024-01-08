2def8b4974fccc3d4938e7bbfe2a74977d78bdff
Samuel Amphoux <amphoux.s@hotmail.fr>
2024-01-08 10:57:50 +0100

    feat: add hover effect to navbar links

--- script.js
+++ script.js
@@ -8,6 +8,12 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const header = document.querySelector('.header');
+const nav = document.querySelector('.nav');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
 
 const openModal = function (e) {
   e.preventDefault();
@@ -52,9 +58,6 @@ message.style.height =
 ///////////////////////////////////////
 // Smooth scrolling
 
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
-
 btnScrollTo.addEventListener('click', function (e) {
   // // For older browsers:
   // const s1coords = section1.getBoundingClientRect();
@@ -95,10 +98,6 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
 ///////////////////////////////////////
 // Tabbed component
 
-const tabs = document.querySelectorAll('.operations__tab');
-const tabsContainer = document.querySelector('.operations__tab-container');
-const tabsContent = document.querySelectorAll('.operations__content');
-
 tabsContainer.addEventListener('click', function (e) {
   // Selects the operations_tab even if a child was clicked
   const target = e.target.closest('.operations__tab');
@@ -138,6 +137,32 @@ tabsContainer.addEventListener('click', function (e) {
       .querySelector(`.operations__content--${Number.parseInt(tabNum)}`)
       .classList.add('operations__content--active');
   }
+});
+
+///////////////////////////////////////
+// Passing arguments to event handlers
+
+nav.addEventListener('mouseover', function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = 0.5;
+    });
+    logo.style.opacity = 0.5;
+  }
+});
+nav.addEventListener('mouseout', function (e) {
+  const link = e.target;
+  const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+  const logo = link.closest('.nav').querySelector('img');
+
+  siblings.forEach(el => {
+    if (el !== link) el.style.opacity = 1;
+  });
+  logo.style.opacity = 1;
 });
 
 ///////////////////////////////////////
